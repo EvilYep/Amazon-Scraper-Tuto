eb23b688a494e8dd6426ef34301b87a1116ed5e9
EvilYep <guillaume.moneret@gmail.com>
2021-07-25 17:52:46 +0200

    first route: product details

--- index.js
+++ index.js
@@ -6,12 +6,26 @@ const app = express();
 const PORT = process.env.PORT;
 
 const apiKey = process.env.API_KEY;
+const baseURl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Welcome to Amazon Scraper API.')
 });
+
+// GET PRODUCT DETAILS
+app.get('/products/:productId', async (req, res) => {
+    const { productId } = req.params;
+
+    try {
+        const response = await request(`${baseURl}&url=https://www.amazon.fr/dp/${productId}`);
+
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error);
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
